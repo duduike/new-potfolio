def583288018bf5e80231b843cffe5a04e0d5d25
duduike <eduardo.happ@yahoo.com>
2023-06-21 18:02:00 -0300

    feat: add initial html structure, header, experien

---
+++ my-portfolio/src/app/components/Experience/experience.tsx
@@ -0,0 +1,31 @@
+import "./experience.scss"
+
+export const Experience = () => {
+    return (
+        <div className="experience">
+            <h3>Experience</h3>
+            <p>text</p>
+            <div className="experience-time">
+
+            </div>
+            <div className="infos">
+                <h3>Languages</h3>
+                <div className="languages-info">
+                    <span>🇺🇸EN - Tecnical</span>
+                    <span>🇧🇷PT-BR - Native Speaker</span>
+                </div>
+                <h3>Education</h3>
+                <div className="educational-info">
+                    <span>🎓</span>
+                    <span>Analysis and Systems Development at Faculdades Metropolitanas Unidas</span>
+                </div>
+            </div>
+            <div className="buttons">
+                <div className="social">
+        
+                </div>
+                <button>contact me</button>
+            </div>
+        </div>
+    )
+}
